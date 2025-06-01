b17de882fd29db9eef3deb6c313a8bf263ea0f54
nGrUnD <teneschevgrund@gmail.com>
2025-06-02 02:15:18 +0700

    fix 2 frontend api

--- VK Service Posting frontend/vk_service_posting/src/api/axios.js
+++ VK Service Posting frontend/vk_service_posting/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://172.18.0.3:8000',
+    baseURL: '/api',
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' },
 });
