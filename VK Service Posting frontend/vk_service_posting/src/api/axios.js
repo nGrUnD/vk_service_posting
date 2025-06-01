32190483a9d60c226c172e2643c9b9174fe2587f
nGrUnD <teneschevgrund@gmail.com>
2025-06-02 02:11:55 +0700

    fox frontend api

--- VK Service Posting frontend/vk_service_posting/src/api/axios.js
+++ VK Service Posting frontend/vk_service_posting/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'http://172.18.0.3:8000',
     withCredentials: true,
     headers: { 'Content-Type': 'application/json' },
 });
