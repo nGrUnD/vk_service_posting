73bee2f2df41d508f0ebb479a4f8040beb731731
nGrUnD <teneschevgrund@gmail.com>
2025-06-01 23:34:36 +0700

    fix celery_config.py

--- VK Service Posting/src/celery_app/celery_config.py
+++ VK Service Posting/src/celery_app/celery_config.py
@@ -1,5 +1,5 @@
-broker_url = 'redis://localhost:6379/0'
-result_backend = 'redis://localhost:6379/1'
+broker_url = 'redis://redis:6379/0'
+result_backend = 'redis://redis:6379/1'
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
