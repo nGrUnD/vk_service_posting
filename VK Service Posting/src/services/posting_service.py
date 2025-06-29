b44bd5d682fac5df80bc6d90302f5ac5835b16b4
nGrUnD <teneschevgrund@gmail.com>
2025-06-29 11:52:21 +0700

    fix post random vk clip per time

--- VK Service Posting/src/services/posting_service.py
+++ VK Service Posting/src/services/posting_service.py
@@ -1,13 +1,13 @@
 import random
 from typing import List, Optional
 
-from src.celery_app import app  # твой celery app
 from src.celery_app.tasks import create_post
 
 from src.repositories.category import CategoryRepository
 from src.repositories.clip_list import ClipListRepository
 from src.repositories.schedule_posting import SchedulePostingRepository
 from src.repositories.vk_account import VKAccountRepository
+from src.repositories.vk_clip import VKClipRepository
 from src.repositories.workerpost import WorkerPostRepository
 from src.schemas.schedule_posting import SchedulePostingAdd, SchedulePostingUpdate
 from src.schemas.vk_clip import VKClipOut
@@ -24,6 +24,7 @@ class PostingService:
         self.category_repo = CategoryRepository(self.session)
         self.schedule_posting = SchedulePostingRepository(self.session)
         self.clip_list = ClipListRepository(self.session)
+        self.vk_clip = VKClipRepository(self.session)
         self.vk_account_repo = VKAccountRepository(self.session)
 
         return self
@@ -62,7 +63,7 @@ class PostingService:
             clip_list = await self.clip_list.get_one_or_none(id=category.clip_list_id)
             vk_account = await self.vk_account_repo.get_one_or_none(id=workpost.vk_account_id)
 
-            clips = clip_list.clips
+            clips = await self.vk_clip.get_all_filtered(clip_list_id=clip_list.id)
 
             random_clip = self.get_random_clip(clips)
             if not random_clip:
