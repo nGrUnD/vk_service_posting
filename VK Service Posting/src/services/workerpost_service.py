fc08df4838d7fe69e9ec207f8f0fa2bfb6b1e344
GrUnD <teneschevgrund@gmail.com>
2025-08-30 11:34:44 +0700

    fix workpost

--- VK Service Posting/src/services/workerpost_service.py
+++ VK Service Posting/src/services/workerpost_service.py
@@ -38,11 +38,11 @@ class WorkerPostService:
         failed_account_log_pass = []
         failed_group_ids = []
 
-        vk_accounts_backup_free = await self.database.vk_account.get_all_filtered(account_type="backup", parse_status="success", flood_control=False)
+        #vk_accounts_backup_free = await self.database.vk_account.get_all_filtered(account_type="backup", parse_status="success", flood_control=False)
 
-        for vk_account_backup_db, vk_group_id in zip(vk_accounts_backup_free, vk_groups_ids):
+        #for vk_account_backup_db, vk_group_id in zip(vk_accounts_backup_free, vk_groups_ids):
+        for vk_group_id in vk_groups_ids:
             category_database = await self.database.category.get_one_or_none(id=request_add.category_id)
-
 
             vk_group_database = await self.database.vk_group.get_one_or_none(vk_group_id=vk_group_id)
             if not vk_group_database:
@@ -52,19 +52,27 @@ class WorkerPostService:
                 #await self.database.commit()
                 continue
 
+            vk_accounts_for_group_db = await self.database.vk_account.get_all_filtered(account_type="backup", parse_status="success", flood_control=False, vk_group_id=vk_group_id)
+            if not vk_accounts_for_group_db:
+                print(f"vk group id нет у backup account: {vk_group_id}")
+                failed_group_ids.append(vk_group_id)
+                continue
+
+            vk_account = vk_accounts_for_group_db[0]
+
             await self.database.vk_account.edit(
                 VKAccountUpdate(account_type="posting"),
                 exclude_unset=True,
-                id=vk_account_backup_db.id
+                id=vk_account.id
             )
 
             await self.database.commit()
 
 
-            password = AuthService().decrypt_data(vk_account_backup_db.encrypted_password)
-            token_db = vk_account_backup_db.token
+            password = AuthService().decrypt_data(vk_account.encrypted_password)
+            token_db = vk_account.token
 
-            proxy_db = await self.database.proxy.get_one_or_none(id=vk_account_backup_db.proxy_id)
+            proxy_db = await self.database.proxy.get_one_or_none(id=vk_account.proxy_id)
             proxy_http = None
             if proxy_db:
                 proxy_http = proxy_db.http
@@ -72,12 +80,12 @@ class WorkerPostService:
             logging.info(f'workerpost_service proxy_http: {proxy_http}')
 
             task = create_workpost_account.delay(
-                account_id_database=vk_account_backup_db.id,
+                account_id_database=vk_account.id,
                 main_account_id_database=main_vk_account_database.id,
                 vk_group_id_database=vk_group_database.id,
                 category_id_database=category_database.id,
                 user_id=user_id,
-                login=vk_account_backup_db.login,
+                login=vk_account.login,
                 password=password,
                 token_db=token_db,
                 proxy=proxy_http
@@ -86,17 +94,17 @@ class WorkerPostService:
             await self.database.vk_account.edit(
                 VKAccountUpdate(task_id=task.id),
                 exclude_unset=True,
-                id=vk_account_backup_db.id
+                id=vk_account.id
             )
 
-            celery_task = await self.database.celery_task.get_one_or_none(vk_account_id=vk_account_backup_db.id, type="add workerpost")
+            celery_task = await self.database.celery_task.get_one_or_none(vk_account_id=vk_account.id, type="add workerpost")
             if not celery_task:
                 # (опционально) создаем celery_task запись
                 celery_task_add = CeleryTaskAdd(
                     task_id=task.id,
                     type="add workerpost",
                     user_id=user_id,
-                    vk_account_id=vk_account_backup_db.id,
+                    vk_account_id=vk_account.id,
                     clip_list_id=None,
                     status="starting"
                 )
@@ -105,13 +113,12 @@ class WorkerPostService:
                 celery_task_update = CeleryTaskUpdate(
                     task_id=task.id,
                     type="add workerpost",
-                    vk_account_id=vk_account_backup_db.id,
+                    vk_account_id=vk_account.id,
                     status="starting"
                 )
                 await self.database.celery_task.edit(celery_task_update, exclude_unset=True, id=celery_task.id)
 
             await self.database.commit()
-
 
         await self.database.commit()
 
