8db3c96af9b9c5d59e6996615e10c644afac2a20
nGrUnD <teneschevgrund@gmail.com>
2026-05-12 11:16:28 +0700

    [dev] fix config

--- VK Service Posting/src/config.py
+++ VK Service Posting/src/config.py
@@ -30,6 +30,6 @@ class Settings(BaseSettings):
 
 
 
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
